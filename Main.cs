7873f7b418e4e157aea6ca0641d52a516841ade4
SDraw <sdraw@protonmail.com>
2020-06-13 14:32:36 +0400

    Detection of SteamVR controllers

--- Main.cs
+++ Main.cs
@@ -28,40 +28,80 @@ namespace ml_mictoggle
         {
             if (m_toggleEnabled)
             {
-                var l_inputProc = VRCInputManager.field_Private_Static_Dictionary_2_EnumNPublicSealedvaKeMoCoGaViOcViDaWaUnique_VRCInputProcessor_0[VRCInputManager.EnumNPublicSealedvaKeMoCoGaViOcViDaWaUnique.Vive].Cast<VRCInputProcessorVive>();
-                if (l_inputProc)
+                SteamVR_ControllerManager l_controllerManager = null;
+                switch (VRCInputManager.prop_EnumNPublicSealedvaKeMoCoGaViOcViDaWaUnique_0)
                 {
-                    var l_controllerManager = l_inputProc.field_Private_SteamVR_ControllerManager_0;
-                    if (l_controllerManager)
-                    {
-                        bool l_buttonNewState = false;
-
-                        // Left hand controller only check
-                        if ((l_controllerManager.field_Private_UInt32_0 != Valve.VR.OpenVR.k_unTrackedDeviceIndexInvalid) && (l_controllerManager.left != null))
+                    case VRCInputManager.EnumNPublicSealedvaKeMoCoGaViOcViDaWaUnique.Vive:
                         {
-                            var l_controller = l_controllerManager.left.GetComponent<SteamVR_TrackedController>();
-                            if (l_controller == null) l_controller = l_controllerManager.left.AddComponent<SteamVR_TrackedController>();
-                            l_controller.Update();
-
-                            l_buttonNewState = l_controller.menuPressed;
-                        }
-
-                        if (m_buttonOldState != l_buttonNewState)
-                        {
-                            m_buttonOldState = l_buttonNewState;
-                            if (m_buttonOldState)
+                            VRCInputProcessor l_input = VRCInputManager.field_Private_Static_Dictionary_2_EnumNPublicSealedvaKeMoCoGaViOcViDaWaUnique_VRCInputProcessor_0[VRCInputManager.EnumNPublicSealedvaKeMoCoGaViOcViDaWaUnique.Vive];
+                            if (l_input)
                             {
-                                long l_tick = System.DateTime.Now.Ticks;
-                                if ((l_tick - m_lastToggleTick) < m_toggleDelay)
-                                {
-                                    m_toggleState = !m_toggleState;
-                                    if (m_toggleState) DefaultTalkController.Method_Public_Static_Void_5();
-                                    else DefaultTalkController.Method_Public_Static_Void_4();
-
-                                    m_lastToggleTick = l_tick - (m_toggleDelay * 2L);
-                                }
-                                else m_lastToggleTick = l_tick;
+                                VRCInputProcessorVive l_castInput = l_input.Cast<VRCInputProcessorVive>();
+                                if (l_castInput) l_controllerManager = l_castInput.field_Private_SteamVR_ControllerManager_0;
                             }
+                        }
+                        break;
+                    case VRCInputManager.EnumNPublicSealedvaKeMoCoGaViOcViDaWaUnique.ViveAdvanced:
+                        {
+                            VRCInputProcessor l_input = VRCInputManager.field_Private_Static_Dictionary_2_EnumNPublicSealedvaKeMoCoGaViOcViDaWaUnique_VRCInputProcessor_0[VRCInputManager.EnumNPublicSealedvaKeMoCoGaViOcViDaWaUnique.ViveAdvanced];
+                            if (l_input)
+                            {
+                                VRCInputProcessorViveAdvanced l_castInput = l_input.Cast<VRCInputProcessorViveAdvanced>();
+                                if (l_castInput) l_controllerManager = l_castInput.field_Private_SteamVR_ControllerManager_0;
+                            }
+                        }
+                        break;
+                    case VRCInputManager.EnumNPublicSealedvaKeMoCoGaViOcViDaWaUnique.Oculus:
+                        {
+                            VRCInputProcessor l_input = VRCInputManager.field_Private_Static_Dictionary_2_EnumNPublicSealedvaKeMoCoGaViOcViDaWaUnique_VRCInputProcessor_0[VRCInputManager.EnumNPublicSealedvaKeMoCoGaViOcViDaWaUnique.Oculus];
+                            if (l_input)
+                            {
+                                VRCInputProcessorTouch l_castInput = l_input.Cast<VRCInputProcessorTouch>();
+                                if (l_castInput) l_controllerManager = l_castInput.field_Private_SteamVR_ControllerManager_0;
+                            }
+                        }
+                        break;
+                    case VRCInputManager.EnumNPublicSealedvaKeMoCoGaViOcViDaWaUnique.Index:
+                        {
+                            VRCInputProcessor l_input = VRCInputManager.field_Private_Static_Dictionary_2_EnumNPublicSealedvaKeMoCoGaViOcViDaWaUnique_VRCInputProcessor_0[VRCInputManager.EnumNPublicSealedvaKeMoCoGaViOcViDaWaUnique.Index];
+                            if (l_input)
+                            {
+                                VRCInputProcessorIndex l_castInput = l_input.Cast<VRCInputProcessorIndex>();
+                                if (l_castInput) l_controllerManager = l_castInput.field_Private_SteamVR_ControllerManager_0;
+                            }
+                        }
+                        break;
+                }
+
+                if (l_controllerManager)
+                {
+                    bool l_buttonNewState = false;
+
+                    // Left hand controller only check
+                    if ((l_controllerManager.field_Private_UInt32_0 != Valve.VR.OpenVR.k_unTrackedDeviceIndexInvalid) && (l_controllerManager.left != null))
+                    {
+                        SteamVR_TrackedController l_controller = l_controllerManager.left.GetComponent<SteamVR_TrackedController>();
+                        if (l_controller == null) l_controller = l_controllerManager.left.AddComponent<SteamVR_TrackedController>();
+                        l_controller.Update();
+
+                        l_buttonNewState = l_controller.menuPressed;
+                    }
+
+                    if (m_buttonOldState != l_buttonNewState)
+                    {
+                        m_buttonOldState = l_buttonNewState;
+                        if (m_buttonOldState)
+                        {
+                            long l_tick = System.DateTime.Now.Ticks;
+                            if ((l_tick - m_lastToggleTick) < m_toggleDelay)
+                            {
+                                m_toggleState = !m_toggleState;
+                                if (m_toggleState) DefaultTalkController.Method_Public_Static_Void_5();
+                                else DefaultTalkController.Method_Public_Static_Void_4();
+
+                                m_lastToggleTick = l_tick - (m_toggleDelay * 2L);
+                            }
+                            else m_lastToggleTick = l_tick;
                         }
                     }
                 }
